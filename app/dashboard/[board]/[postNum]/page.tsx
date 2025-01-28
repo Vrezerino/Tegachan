bbac207e0ad4aef204639cf08f73f59fa99d0e74
Patrick <parkpatrick1@gmail.com>
2025-01-28 16:28:36 +0200

    Edit board and post params types

--- app/dashboard/[board]/[postNum]/page.tsx
+++ app/dashboard/[board]/[postNum]/page.tsx
@@ -3,21 +3,16 @@ import { getPost } from './data';
 import Post from '@/app/ui/dashboard/post';
 import { notFound, redirect } from 'next/navigation';
 
-type PostParams = {
-    params: {
-        board: string;
-        postNum: number;
-    }
-};
+type ParamsType = Promise<{ board: string; postNum: string }>;
 
-const Page = async ({ params }: PostParams) => {
+const Page = async ({ params }: { params: ParamsType }) => {
     // Asynchronous access of params is now necessary
     const { board, postNum } = await params;
 
     const data: PostType = await getPost(board, postNum.toString());
     if (Object.keys(data).length === 0) notFound();
 
-    /* 
+    /**
       If the post is not OP (thread starter), don't allow single post view,
       redirect to parent thread instead 
     */
--- app/dashboard/[board]/page.tsx
+++ app/dashboard/[board]/page.tsx
@@ -5,21 +5,17 @@ import Board from '@/app/ui/dashboard/board';
 import { notFound } from 'next/navigation';
 import { boards, sanitizeString, toTitleCase } from '@/app/lib/utils';
 
-type BoardParams = {
-    params: {
-        board: string;
-    }
-};
+type ParamsType = Promise<{ board: string; }>;
 
-const Page = async ({ params }: BoardParams) => {
+const Page = async ({ params }: { params: ParamsType }) => {
     // Asynchronous access of params is now necessary
     const { board } = await params;
+    const sanitizedBoardName = sanitizeString(board);
 
     // Get thread starter posts from given board
-    const data: PostType[] = await getBumpedPosts(board);
+    const data: PostType[] = await getBumpedPosts(sanitizedBoardName);
 
     // Render 404 page if board name in request doesn't refer to an existing board
-    const sanitizedBoardName = sanitizeString(board);
     const regex = new RegExp('\\b' + sanitizedBoardName + '\\b');
     if (!regex.test(boards)) notFound();
 
