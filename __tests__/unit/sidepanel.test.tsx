25e514075d55881da64bdf0cb7e7d66e658cef0f
Patrick <parkpatrick1@gmail.com>
2025-02-11 16:01:03 +0200

    Edit sidepanel unit test

--- __tests__/unit/sidepanel.test.tsx
+++ __tests__/unit/sidepanel.test.tsx
@@ -15,55 +15,68 @@ describe('Navigation', () => {
         render(<NavLinks />);
 
         // Index
-        const indexParagraph = screen.getAllByText('Index')[0];
-        expect(indexParagraph.closest('p')).toBeDefined();
+        const indexParagraph = screen.getAllByText('Index')[0].closest('p');
+        const indexLink = indexParagraph?.closest('a');
+        expect(indexLink?.getAttribute('href')).toBe('/dashboard');
 
         // Technology
-        const techParagraph = screen.getAllByText('Technology')[0];
-        expect(techParagraph.closest('p')).toBeDefined();
+        const techParagraph = screen.getAllByText('Technology')[0].closest('p');
+        const techLink = techParagraph?.closest('a');
+        expect(techLink?.getAttribute('href')).toBe('/dashboard/technology');
 
         // Business
-        const bizParagraph = screen.getAllByText('Business')[0];
-        expect(bizParagraph.closest('p')).toBeDefined();
+        const bizParagraph = screen.getAllByText('Business')[0].closest('p');
+        const bizLink = bizParagraph?.closest('a');
+        expect(bizLink?.getAttribute('href')).toBe('/dashboard/business');
 
         // Music
-        const musicParagraph = screen.getAllByText('Music')[0];
-        expect(musicParagraph.closest('p')).toBeDefined();
+        const musicParagraph = screen.getAllByText('Music')[0].closest('p');
+        const musicLink = musicParagraph?.closest('a');
+        expect(musicLink?.getAttribute('href')).toBe('/dashboard/music');
 
         // Art
-        const artParagraph = screen.getAllByText('Art')[0];
-        expect(artParagraph.closest('p')).toBeDefined();
+        const artParagraph = screen.getAllByText('Art')[0].closest('p');
+        const artLink = artParagraph?.closest('a');
+        expect(artLink?.getAttribute('href')).toBe('/dashboard/art');
 
         // Video games
-        const vidyaParagraph = screen.getAllByText('Video Games')[0];
-        expect(vidyaParagraph.closest('p')).toBeDefined();
+        const vidyaParagraph = screen.getAllByText('Video Games')[0].closest('p');
+        const vidyaLink = vidyaParagraph?.closest('a');
+        expect(vidyaLink?.getAttribute('href')).toBe('/dashboard/videogames');
 
         // TV & Film
-        const tvParagraph = screen.getAllByText('TV & Film')[0];
-        expect(tvParagraph.closest('p')).toBeDefined();
+        const tvParagraph = screen.getAllByText('TV & Film')[0].closest('p');
+        const tvLink = tvParagraph?.closest('a');
+        expect(tvLink?.getAttribute('href')).toBe('/dashboard/tv');
 
         // Automobiles
-        const autoParagraph = screen.getAllByText('Automobiles')[0];
-        expect(autoParagraph.closest('p')).toBeDefined();
+        const autoParagraph = screen.getAllByText('Automobiles')[0].closest('p');
+        const autoLink = autoParagraph?.closest('a');
+        expect(autoLink?.getAttribute('href')).toBe('/dashboard/auto');
 
         // Outdoors
-        const outParagraph = screen.getAllByText('Outdoors')[0];
-        expect(outParagraph.closest('p')).toBeDefined();
+        const outParagraph = screen.getAllByText('Outdoors')[0].closest('p');
+        const outLink = outParagraph?.closest('a');
+        expect(outLink?.getAttribute('href')).toBe('/dashboard/outdoors');
 
         // Sports
-        const sportsParagraph = screen.getAllByText('Sports')[0];
-        expect(sportsParagraph.closest('p')).toBeDefined();
+        const sportsParagraph = screen.getAllByText('Sports')[0].closest('p');
+        const sportsLink = sportsParagraph?.closest('a');
+        expect(sportsLink?.getAttribute('href')).toBe('/dashboard/sports');
 
         // Science & Math
-        const scienceParagraph = screen.getAllByText('Science & Math')[0];
-        expect(scienceParagraph.closest('p')).toBeDefined();
+        const scienceParagraph = screen.getAllByText('Science & Math')[0].closest('p');
+        const scienceLink = scienceParagraph?.closest('a');
+        expect(scienceLink?.getAttribute('href')).toBe('/dashboard/science');
 
         // International
-        const intParagraph = screen.getAllByText('International')[0];
-        expect(intParagraph.closest('p')).toBeDefined();
+        const intParagraph = screen.getAllByText('International')[0].closest('p');
+        const intLink = intParagraph?.closest('a');
+        expect(intLink?.getAttribute('href')).toBe('/dashboard/international');
 
         // Random
-        const randParagraph = screen.getAllByText('Random')[0];
-        expect(randParagraph.closest('p')).toBeDefined();
+        const randParagraph = screen.getAllByText('Random')[0].closest('p');
+        const randLink = randParagraph?.closest('a');
+        expect(randLink?.getAttribute('href')).toBe('/dashboard/random');
     });
 });
