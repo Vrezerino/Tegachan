3416d8a759b507f7bd4a657f698a72aac2107ce9
Patrick <parkpatrick1@gmail.com>
2025-02-15 20:35:48 +0200

    Remove text next to theme toggle; Edit toggle useEffect func

--- app/ui/dashboard/sideNav/index.tsx
+++ app/ui/dashboard/sideNav/index.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 const SideNav = () => {
   // Toggle darkmode on website
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
 
     if (e.target.checked) {
       window.localStorage.setItem('theme', 'dark');
@@ -21,15 +21,16 @@ const SideNav = () => {
    * TailwindCSS selectors starting with "dark:" will be used throughout the site
    */
   useEffect(() => {
+    const toggleInput = document.getElementById('toggle') as HTMLInputElement | null;
+    if (!toggleInput) return;
+
     const theme = window.localStorage.getItem('theme');
 
     if (theme) {
       document.documentElement.classList.add(theme);
 
-      // Keep toggle input toggled if theme has been set to be dark
-      if (theme === 'dark') {
-        (document.getElementById('toggle') as HTMLInputElement).checked = true;
-      }
+      // Retain dark mode if set previously
+      theme === 'dark' && (toggleInput!.checked = true);
     }
   }, [])
 
@@ -40,9 +41,8 @@ const SideNav = () => {
       </div>
 
       <label className='flex justify-center xsm:justify-end cursor-pointer md:mt-4 dark:label-darkmode'>
-        <input id='toggle' type='checkbox' value='' className='sr-only peer' onChange={onChange} />
+        <input id='toggle' type='checkbox' value='' className='sr-only peer' onChange={toggleTheme} />
         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-400/40"></div>
-        <span className='ms-3 text-sm font-medium text-gray-900 dark:text-gray-300'>Dark mode</span>
       </label>
     </div>
   );
