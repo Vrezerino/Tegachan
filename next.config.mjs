584bd1f636c7f9d7bbd998e585bf857873995ef9
Patrick <parkpatrick1@gmail.com>
2025-05-07 17:06:29 +0300

    Replace Vercel with Azure VMs; Edit CI/CD pipeline (reverse testing and building; add deployment job)

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '*',
-                port: '',
-                pathname: '/**/**',
-            },
-        ],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/dashboard',
-                permanent: true,
-            }
-        ]
-    }
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "/**/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
