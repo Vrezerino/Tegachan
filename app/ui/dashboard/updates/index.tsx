351c7fae2cf924e889001edfe44735344a31f6a3
Patrick <parkpatrick1@gmail.com>
2025-06-10 00:21:44 +0300

    Fix (UI): updates section data-testid

--- app/ui/dashboard/updates/index.tsx
+++ app/ui/dashboard/updates/index.tsx
@@ -2,9 +2,9 @@
 
 const Updates = () => {
   return (
-    <div className='mx-auto mb-8 w-full max-w-md p-2 bg-white border border-neutral-200 rounded-xs rounded-br-2xl shadow-sm sm:p-3 dark:bg-neutral-900 dark:border-neutral-800'>
+    <div className='mx-auto mb-8 w-full max-w-md p-2 bg-white border border-neutral-200 rounded-xs rounded-br-2xl shadow-sm sm:p-3 dark:bg-neutral-900 dark:border-neutral-800' data-testid='updates'>
       <h5 className='mb-5 mx-auto text-xl text-center font-bold leading-none text-neutral-900 dark:header-darkmode'>UPDATES</h5>
-      <p className='text-sm font-medium text-neutral-900 dark:text-white' data-testid='total-posts-count'>
+      <p className='text-sm font-medium text-neutral-900 dark:text-white' data-testid='update'>
         <b>09 Jun 2025</b>: Post num incrementation was dysfunctional after last downtime — posting is again possible
       </p>
     </div>
