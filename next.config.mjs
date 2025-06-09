021d34ea701335717474eace261828c059905ac2
Patrick <parkpatrick1@gmail.com>
2025-06-09 22:35:03 +0300

    Edit (next.config): add more imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   webpack(config) {
